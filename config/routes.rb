78a271e1aa57556888cd1a27ab3f1e0f85faff61
Jeewan Rana <jeewanrana@SV-LT-1670.local>
2020-03-01 09:20:24 -0800

    Add search friends form

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   end
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
-  get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
   get 'chart', to: 'stocks#chart'
 end
